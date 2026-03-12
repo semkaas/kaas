8ff4014db56a7334e205f691f50abffd65e4027d
semkaas <6052414@mborijnland.nl>
2026-03-12 13:57:53 +0100

    index verbeterd

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
-<?php
-echo "index pagina";
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="javascript/script.js"></script>
+    
+</head>
+<body>
+
+<?php include 'includes/header.php'; ?>
 
 
-?>
+ <section class="deals">
+        <article class="deal1">
+            <h1> wekelijkse deals </h1>
+            <img src="img/hamlappen.png" alt="fotohamlap">
+        </article>
+        <article class="deal2">
+            <h1> beef brisket 12% korting </h1>
+            <img src="img/patat.webp" alt="fotopatat">
+            <h1> patat nu 8% korting </h1>
+        </article>
+ </section>
+    <hr>
 
+
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
 
