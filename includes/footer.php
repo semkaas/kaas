2c76a0018bebbb1473af76e3c6bef17f1063b063
semkaas <6052414@mborijnland.nl>
2026-03-17 12:43:49 +0100

    footer uitgebreid

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,14 @@
 <footer>
-    <section class="footer">
+    <section class="voet">
         <article>
-            <h1>dit is een footer</h1>
+            <h1>contact</h1>
+            <h2>vlam en vlees</h2>
+            <h2>promenade 12</h2>
+            <h2>2711AB Zoetermeer </h2>
+            <h2>info@vlamenvleeszoetemeer.nl</h2>
+            <h2>088-1321111</h2>
+            <h2>© copyright vlam en vlees.</h2>
+            <h2>Alle rechten voorbehouden</h2>
         </article>
     </section>
 </footer>
