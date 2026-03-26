0e2a27a13dbacad3f44c852e1dc067a2dcc0c343
SidneyEmen <6058166@mborijnland.nl>
2026-03-26 13:46:46 +0100

    Navigatie Anders gedaan

--- navigatie.php
+++ navigatie.php
@@ -12,19 +12,12 @@
 <?php include 'includes/header.php'; ?>
 <header></header>
 <main>
-    <section id="OpeningsTijden">
-        <article class="Tijden">
-            <h2 class = "Week">Openings tijden deze week:</h2>
-            <h2>Maandag: 11:00 - 21:30</h2>
-            <h2>Dinsdag: 11:00 - 21:30</h2>
-            <h2>Woensdag: 11:00 - 21:30</h2>
-            <h2>Donderdag: 11:00 - 21:30</h2>
-            <h2>Vrijdag: 11:00 - 21:30</h2>
-            <h2>Zaterdag: 11:00 - 21:30</h2>
-            <h2>Zondag: 11:00 - 21:30</h2>
-           
+    <section>
+        <article class="Locatie">
+            <h1>Locatie</h1>
         </article>
-</section>
+    </section>
+ 
 </main>
 <?php include 'includes/footer.php'; ?>
 </body>
