cc36c96fc71330444857a15b941cd3d73c76b4c7
semkaas <6052414@mborijnland.nl>
2026-03-26 13:15:30 +0100

    Update vacatures.php

--- vacatures.php
+++ vacatures.php
@@ -1,3 +1,19 @@
-<?php
- echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="javascript/script.js" defer></script>
+   
+</head>
+<body>
+<?php include 'includes/header.php'; ?>
+<header></header>
+<main>
+
+</main>
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
