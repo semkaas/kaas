72ce05a1f030abb7292a002766ea133133c10363
semkaas <6052414@mborijnland.nl>
2026-03-31 12:29:46 +0200

    optimalisation

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
-let lightanddark = document.getElementById("lightdark")
-document.documentElement.dataset.mode = localStorage.getItem("theme")
+let lightanddark = document.getElementById("lightdark");
+document.documentElement.dataset.mode = localStorage.getItem("theme");
 
 
 
@@ -8,40 +8,22 @@ function toggleMenu() {
     menu.classList.toggle('open');
 }
 
-// const ham = document.getElementsByClassName("hamburger")[0]
-// const nav = document.getElementsByClassName("banner")[0]
-// const main = document.main;
-
-// // const lightdark = document.getElementById('darkmode')[0]
-// function showOrHideNav(e){
-//     if(nav.style.display != 'flex'){
-//         nav.style.display = 'flex'
-//     }else{
-//         main.style.display = 'none'
-//     }
-// }
-// ham.addEventListener('click', showOrHideNav);
 function switchMode(){
     if(document.documentElement.dataset.mode != "dark"){
        document.documentElement.dataset.mode = 'dark';
-       localStorage.setItem("theme", "dark")
+       localStorage.setItem("theme", "dark");
 }
 else{
-    document.documentElement.dataset.mode = "light"
-    localStorage.setItem("theme", "light")
+    document.documentElement.dataset.mode = "light";
+    localStorage.setItem("theme", "light");
 }
     
 }
 
-// document.element
 lightanddark.addEventListener('click', switchMode);
-// (document.documentElement.dataset.mode = 'dark'){
-//         document.docu
+
 function changeFontSize(delta) {
   const root = document.documentElement;
-  // Haal de huidige schaal op (standaard 1)
   const currentScale = parseFloat(getComputedStyle(root).getPropertyValue('--global-scale'));
-  
-  // Verhoog of verlaag de schaal (bijv. met 0.1 per klik)
   root.style.setProperty('--global-scale', currentScale + (delta * 0.1));
 }
